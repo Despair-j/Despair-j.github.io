b7e990fc145d3c0d22d2729eebe6a5c25998cc25
Despair-j <Dispair.123@outlook.com>
2026-08-06 16:39:11 +0800

    Site updated: 2026-08-06 16:39:09

---
+++ js/title.js
@@ -0,0 +1,19 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '想我了没有📞~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖抓到你啦～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+
+
